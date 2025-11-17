dad84e31744f46ae2f1ea070023d82a0dc2c9e7a
Gabriel Estrela <gabrielestrela1009@hotmail.com>
2025-11-17 17:15:36 -0300

    feat: initialize Vue 3 project with Vite, add basic structure and components
    - Add favicon.ico for the application.
    - Create main application component (App.vue) with routing setup.
    - Add global CSS styles (base.css) for theming and layout.
    - Include logo.svg for branding.
    - Set up Tailwind CSS and animation imports in main.css.
    - Implement HelloWorld component to display a greeting message.
    - Create TheWelcome component with multiple WelcomeItem components for project guidance.
    - Add WelcomeItem component to structure welcome messages.
    - Introduce various icon components for documentation, tooling, ecosystem, community, and support.
    - Create reusable Button component with variants and styles.
    - Implement HoverCard component for interactive UI elements.
    - Develop Slider component for user input.
    - Set up Pinia store for state management with a counter example.
    - Create HomeView and AboutView components for routing.
    - Configure TypeScript settings for the project.
    - Set up Vite configuration for Vue and Tailwind CSS integration.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import { Button } from '@/components/ui/button';
+import { ArrowUpIcon } from 'lucide-vue-next'
+import YoutubeMusic from '../components/global/YoutubeMusic.vue';
+</script>
+
+<template>
+  <main>
+
+    <YoutubeMusic />
+  </main>
+</template>
