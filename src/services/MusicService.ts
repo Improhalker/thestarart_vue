35f0ab9def3ef336fd56adcf798f944885b273ae
Gabriel Estrela <gabrielestrela1009@hotmail.com>
2026-04-05 00:57:54 -0300

    fix: adding https to baseUrl

--- src/services/MusicService.ts
+++ src/services/MusicService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://srv906410.hstgr.cloud/api',
+  baseURL: 'https://srv906410.hstgr.cloud/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
