5bc7167f46c1986c301985e754984c07b2cc5cc9
Gabriel Estrela <gabrielestrela1009@hotmail.com>
2026-04-05 02:15:38 -0300

    fix: update baseURL in MusicService to use environment variable

--- src/services/MusicService.ts
+++ src/services/MusicService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://srv906410.hstgr.cloud/api', 
+  baseURL: import.meta.env.VITE_API_URL, 
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
