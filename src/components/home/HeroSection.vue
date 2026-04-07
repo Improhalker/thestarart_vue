28c294500dd4308938fb2a21af00a2c553609f89
Gabriel Estrela <gabrielestrela1009@hotmail.com>
2026-04-07 17:59:20 -0300

    fix: adjusting container

--- src/components/home/HeroSection.vue
+++ src/components/home/HeroSection.vue
@@ -8,8 +8,8 @@ import Main from './Main.vue';
 </script>
 
 <template>
-  <section class="w-full min-h-screen bg-[#13011a] text-pink-100">
-    <div class="h-full grid grid-cols-1 md:grid-cols-[1fr_2fr_1fr] gap-2 lg:gap-6 p-6">
+  <section class=" min-h-screen bg-[#13011a] text-pink-100">
+    <div class="2xl:w-[1600px] place-self-center h-full grid grid-cols-1 md:grid-cols-[1fr_2fr_1fr] gap-2 lg:gap-6 p-6">
       <LeftSide />
       <div class="rounded-xl 00 p-2 ">
         <Main />
