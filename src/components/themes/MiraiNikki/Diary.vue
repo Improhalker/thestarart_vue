956844c3e46878c90ef9c82f5d82989ecc5db7a5
Gabriel Estrela <gabrielestrela1009@hotmail.com>
2026-06-10 17:39:31 -0300

    fix border radius in the diary component

--- src/components/themes/MiraiNikki/Diary.vue
+++ src/components/themes/MiraiNikki/Diary.vue
@@ -6,7 +6,9 @@
     <div
       class="flex flex-col w-72 h-[980px] diary-blue rounded-3xl border-4 border-[#21454c] place-self-center mx-auto my-10"
     >
-      <div class="h-full px-5 pt-8 pb-0 border-4 rounded-2xl border-[#21454c]">
+      <div
+        class="h-full px-5 pt-8 pb-0 border-4 rounded-2xl border-[#21454c] rounded-b-none"
+      >
         <div
           class="bg-gray-800 w-full h-full rounded-b-none py-4 px-2 border-black border-2"
         >
