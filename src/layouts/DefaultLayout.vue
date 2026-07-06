8a1d0a4b3094c884b4f07eca7863df1b740846b3
Gabriel Estrela <gabrielestrela1009@hotmail.com>
2026-07-06 14:03:12 -0300

    feat: update social media links in DefaultLayout and add email icon

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -16,7 +16,7 @@ import AdminPet from "@/components/admin/madoka/AdminPet.vue";
     >
       <section class="px-0 pb-4 lg:pb-16 pt-0 lg:pt-24">
         <div
-          class="container hidden md:flex mx-auto lg:mb-8 items-center gap-4 py-1 px-2 text-[11px] text-white"
+          class="container flex py-4 mx-auto lg:mb-8 justify-center items-center gap-4 lg:py-1 px-2 text-[11px] bg-purple-200/30 text-white"
         >
           <a
             href="https://www.instagram.com/thestarart_/"
@@ -24,15 +24,16 @@ import AdminPet from "@/components/admin/madoka/AdminPet.vue";
             class="flex items-center gap-1 hover:text-white transition"
           >
             <img src="/images/social/instagram.png" class="w-4 h-4 pixelated" />
-            Instagram
+            <p class="hidden md:flex">Instagram</p>
           </a>
+
           <a
             href="https://youtube.com/@improhalkeryg254"
             target="_blank"
             class="flex items-center gap-1 hover:text-white transition"
           >
-            <img src="/images/social/youtube.png" class="w-4 h-4 pixelated" />
-            Arte e Filosofia
+            <img src="/images/social/youtube.png" class="w-5 h-4 pixelated" />
+            <p class="hidden md:flex">Arte e Filosofia</p>
           </a>
 
           <a
@@ -40,8 +41,8 @@ import AdminPet from "@/components/admin/madoka/AdminPet.vue";
             target="_blank"
             class="flex items-center gap-1 hover:text-white transition"
           >
-            <img src="/images/social/youtube.png" class="w-4 h-4 pixelated" />
-            Jogos
+            <img src="/images/social/youtube.png" class="w-5 h-4 pixelated" />
+            <p class="hidden md:flex">Jogos</p>
           </a>
 
           <a
@@ -50,7 +51,7 @@ import AdminPet from "@/components/admin/madoka/AdminPet.vue";
             class="flex items-center gap-1 hover:text-white transition"
           >
             <img src="/images/social/tiktok.png" class="w-4 h-4 pixelated" />
-            TikTok
+            <p class="hidden md:flex">TikTok</p>
           </a>
 
           <a
@@ -59,14 +60,16 @@ import AdminPet from "@/components/admin/madoka/AdminPet.vue";
             class="flex items-center gap-1 hover:text-white transition"
           >
             <img src="/images/social/github.png" class="w-4 h-4 pixelated" />
-            GitHub
+            <p class="hidden md:flex">GitHub</p>
           </a>
 
           <a
             href="mailto:contato@contact.thestarart@gmail.com"
-            class="flex items-center gap-1 hover:text-white transition"
+            class="flex items-center gap-1 text-black hover:text-white transition"
           >
-            ✉ Email
+            <img src="/images/social/mail.png" class="w-5 h-4 pixelated" />
+
+            <p class="hidden md:flex text-white">Email</p>
           </a>
         </div>
         <Navbar />
